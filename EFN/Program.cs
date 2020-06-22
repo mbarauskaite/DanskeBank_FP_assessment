421b36babe0b823be23334960aea1cc046d23829
Monika.Barauskaite <mbarauskaite@unibz.it>
2020-06-22 09:53:20 +0200

    Even Fibonacci Numbers Task

---
+++ EFN/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EFN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[1000000];
+            int i = 2;
+            arr[i - 2] = 1;
+            arr[i - 1] = 2;
+            int n = arr[i];
+            long sum = 0;
+
+            for(i=2; arr[i-1]<=4000000; i++)
+            {
+                arr[i] = arr[(i - 1)] + arr[(i - 2)];
+            }
+            for(int f = 0; f<=arr.Length -1; f++)
+            {
+                if (arr[f] % 2 == 0)
+                    sum += arr[f];
+            }
+            Console.WriteLine(sum); //answer: 4613732
+        }
+    }
+}
